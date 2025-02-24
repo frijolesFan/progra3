c9e30da13e6ed3a7950f69b447aa4c63b000cc91
frijolesFan <juan.guevara3@utp.edu.co>
2025-02-24 10:02:35 -0500

    relaciones añadidas

--- actividad01/simpsons.pl
+++ actividad01/simpsons.pl
@@ -1,3 +1,19 @@
-%comentario
+%programa con git
 
 padre(homero, bart).
+padre(homero, lisa).
+padre(homero, maggie).
+padre(abraham, herbert).
+padre(abraham, homero).
+padre(clancy, marge).
+padre(clancy, patty).
+padre(clancy, selma).
+madre(mona, homero).
+madre(marge, bart).
+madre(marge, lisa).
+madre(marge, maggie).
+madre(jacqueline, patty).
+madre(jacqueline, selma).
+madre(jacqueline, marge).
+madre(selma, ling).
+
