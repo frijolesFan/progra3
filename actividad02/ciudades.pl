331276d88b748d93fad45e6440a601a54f9367cc
frijolesFan <juan.guevara3@utp.edu.co>
2025-02-27 09:32:34 -0500

    problema de las ciudades terminado, haciendo uso de listas en prolog para evitar la recursion infinita

--- actividad02/ciudades.pl
+++ actividad02/ciudades.pl
@@ -17,3 +17,19 @@ tiene_aristas(X) :-
 costo_nodoXYZ(X, Z, C) :-
     conexion(X, Y, C1), conexion(Y, Z, C2),
     C is C1 + C2.
+
+%muestra todos los nodos con los que tiene conexion (aristas)
+aristas(X, Y) :-
+    findall((Z, C), conexion(X, Z, C), Y).
+
+%caso base
+camino(I, I, 0, [(I, 0)], _) :- !.
+
+%inicio, final, coste, camino, ciudades visitadas
+%consulta: (ciudad de inicio, ciudad final, C, Y, [ciudad de inicio])
+camino(I, F, C, [(I, C1)|Camino], Visitados) :-
+    conexion(I, M, C1),
+    \+ member(M, Visitados),
+    camino(M, F, C2, Camino, [M|Visitados]),
+    C is C1 + C2,
+    !.
