a8dd9fb4f8bcf84adbd318bda1f0bdbd8718239f
frijolesFan <juan.guevara3@utp.edu.co>
2025-03-28 17:42:53 -0500

    reglas finalizadas

--- actividad05/actividad05.pl
+++ actividad05/actividad05.pl
@@ -1,3 +1,4 @@
+%relaciones directas----------------------------
 padre(homero, [bart, lisa, maggie]).
 padre(abraham, [herbert, homero]).
 padre(clancy, [marge, patty, selma]).
@@ -7,6 +8,7 @@ madre(marge, [bart, lisa, maggie]).
 madre(jacqueline, [marge, patty, selma]).
 madre(selma, [ling]).
 
+% Definición de relaciones hijo/a usando listas de padres
 hijo(herbert, [abraham, na]).
 hijo(homero, [abraham, mona]).
 hijo(bart, [homero, marge]).
@@ -17,30 +19,33 @@ hija(lisa, [homero, marge]).
 hija(maggie, [homero, marge]).
 hija(ling, [na, selma]).
 
-%relaciones indirectas --------------
-
+%relaciones indirectas -------------------------
 abuelo(X, Y) :-
-    padre(X, Z), (padre(Z, Y); madre(Z, Y)).
+    padre(X, Hijos), member(Z, Hijos), (padre(Z, Nietos), member(Y, Nietos); madre(Z, Nietos), member(Y, Nietos)).
 
-abuela(X, Y) :- 
-    madre(X, Z), (padre(Z, Y); madre(Z, Y)).
+abuela(X, Y) :-
+    madre(X, Hijos), member(Z, Hijos), (padre(Z, Nietos), member(Y, Nietos); madre(Z, Nietos), member(Y, Nietos)).
 
 hermano(X, Y) :-
-    (hijo(Y, P, M); hija(Y, P, M)), ((hijo(X, P, _); !); (hijo(X, _, M); !)),
+    (hijo(Y, [P, M]); hija(Y, [P, M])),
+    ((hijo(X, [P, _]); hijo(X, [_, M])); !),
     X \= Y.
 
 hermana(X, Y) :-
-    (hijo(Y, P, M); hija(Y, P, M)), ((hija(X, P, _); !); (hija(X, _, M); !)),
+    (hijo(Y, [P, M]); hija(Y, [P, M])),
+    ((hija(X, [P, _]); hija(X, [_, M])); !),
     X \= Y.
 
 tio(X, Y) :-
-    (padre(Z, Y), hermano(X, Z)) ; (madre(Z, Y), hermano(X, Z)).
+    (padre(Z, Hijos), member(Y, Hijos), hermano(X, Z));
+    (madre(Z, Hijos), member(Y, Hijos), hermano(X, Z)).
 
 tia(X, Y) :-
-    (padre(Z, Y), hermana(X, Z)) ; (madre(Z, Y), hermana(X, Z)).
+    (padre(Z, Hijos), member(Y, Hijos), hermana(X, Z));
+    (madre(Z, Hijos), member(Y, Hijos), hermana(X, Z)).
 
 primo(X, Y) :-
-    (tia(T, Y); tio(T, Y)), hijo(X, _, T).
+    (tia(T, Y); tio(T, Y)), hijo(X, _, [T | _]).
 
 prima(X, Y) :-
-    (tia(T, Y); tio(T, Y)), hija(X, _, T).
+    (tia(T, Y); tio(T, Y)), hija(X, _, [T | _]).
