e610579d7af7e36bf0ac78226bc4d7ffc6918c10
frijolesFan <juan.guevara3@utp.edu.co>
2025-05-15 16:25:53 -0500

    asasd

--- python/sudoku/sudokufinal.py
+++ python/sudoku/sudokufinal.py
@@ -1,25 +1,54 @@
 #genera cada celda del sudoku
+
+debug = True
+ntab = 1
+
+def separador():
+  print('\n',"-"*50,'\n')
+  return ''
+
 cols="ABCDEFGHI"
-rows={i for i in range(1,10)}
+filas={i for i in range(1,10)}
 
 #asigna un dominio a cada celda
 Vars={}
 for col in cols:
-  for row in rows:
-    Vars[f"{col}{row}"]=rows.copy()
+  for fila in filas:
+    Vars[f"{col}{fila}"]=filas.copy()
+
+print(f"{separador()}Genera el dominio de cada celda:\n\n{Vars}") if debug else None
 
 #genera las restricciones para filas y columnas
 const=[]
 for col in cols:
   vars=set()
-  for row in rows:
-    vars.add(f"{col}{row}")
+  for fila in filas:
+    vars.add(f"{col}{fila}")
   const.append(vars)
 
-for row in rows:
+for fila in filas:
   vars=set()
   for col in cols:
-    vars.add(f"{col}{row}")
+    vars.add(f"{col}{fila}")
   const.append(vars)
 
-print(Vars)
+print(f"{separador()}Genera las restricciones por columna y por fila:\n\n{const}") if debug else None
+
+#genera las restricciones para cada cuadro 3x3
+
+for box_x in range(0, 9, 3): 
+    for box_y in range(0, 9, 3):
+        vars = set()
+        for row in range(1 + box_x, 4 + box_x):
+            for col in "ABCDEFGHI"[box_y:box_y + 3]:
+                vars.add(f"{col}{row}")
+        const.append(vars)
+
+print(f"{separador()}Genera la Restricción por cajas:\n\n{const}") if debug else None
+
+with open(f'tab{ntab}.txt') as fd: 
+  for fila in filas:
+    for col in cols:
+      value=int(fd.readline())
+      if value<10 and value:
+        Vars[f"{col}{fila}"]={value}
